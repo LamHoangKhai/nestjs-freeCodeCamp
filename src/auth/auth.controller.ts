a3a0fb6b0a5b9b8b5cdcb9025a7769917421f2ef
lamhoangkhai <heoloveninh2014@gmail.com>
2023-11-02 22:06:06 +0700

    NestJS 2/11

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
+
+@Controller('auth') // derlace  path prefix
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signup(@Body() dto: AuthDto) {
+    console.log({
+      dto,
+    });
+    return this.authService.signup(dto);
+  }
+
+  @Post('signin')
+  signin() {
+    return this.authService.signin();
+  }
+}
---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,18 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+} from 'class-validator';
+
+class AuthDto {
+  @IsEmail()
+  @IsNotEmpty()
+  
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
+
+export { AuthDto };
